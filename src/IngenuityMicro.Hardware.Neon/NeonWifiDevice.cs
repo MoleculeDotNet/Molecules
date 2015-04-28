01349527869c01e05c0f3589d697c34986704fd1
Martin Calsyn <martin@prometheusengineering.net>
2015-04-28 18:21:22 +0300

    Missing file

---
+++ src/IngenuityMicro.Hardware.Neon/NeonWifiDevice.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using System.IO.Ports;
+using IngenuityMicro.Hardware.ESP8266;
+
+namespace IngenuityMicro.Hardware.Neon
+{
+    public sealed class NeonWifiDevice : Esp8266WifiDevice
+    {
+        public NeonWifiDevice()
+            : this("COM2")
+        {
+        }
+
+        public NeonWifiDevice(string comPortName) :
+            base(new SerialPort(comPortName, 115200, Parity.None, 8, StopBits.One), Oxygen.Hardware.RfPower, null)
+        {
+        }
+
+        public NeonWifiDevice(string comPortName, OutputPort resetPin) :
+            base(new SerialPort(comPortName, 115200, Parity.None, 8, StopBits.One), Oxygen.Hardware.RfPower, resetPin)
+        {
+        }
+
+    }
+}
